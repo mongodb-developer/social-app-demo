04e8df35213fe0f457fc8739ee8924fec7697ebb
codeSTACKr <codestackr@gmail.com>
2022-05-14 19:36:26 -0500

    restructure

--- components/Profile/Profile.js
+++
@@ -1,9 +0,0 @@
-
-const Profile = () => {
-  return (
-    <>
-    </>
-  );
-}
-
-export default Profile
--- pages/api/auth/[...auth0].js
+++
@@ -1,3 +0,0 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
-
-export default handleAuth();
