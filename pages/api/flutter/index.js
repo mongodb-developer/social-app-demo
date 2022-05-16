0e42dec3f028c70f183b1bc25c220cbc7246a261
codeSTACKr <codestackr@gmail.com>
2022-05-15 20:12:34 -0500

    add endpoint and function overview

--- pages/api/flutter/index.js
+++ pages/api/flutter/index.js
@@ -1,11 +1,72 @@
 export default async function handler(req, res) {
-  const fetchOptions = {};
-  const fetchBody = {};
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.MONGODB_DATA_API_KEY,
+    },
+  };
+  const fetchBody = {
+    dataSource: "Cluster0",
+    database: "social_butterfly",
+    collection: "flutters",
+  };
   const baseUrl =
-    "https://data.mongodb-api.com/app/<your-data-api-name>/endpoint/data/beta/action";
+    "https://data.mongodb-api.com/app/<Data API App ID>/endpoint/data/beta/action";
 
   try {
     switch (req.method) {
+      case "GET":
+        const readData = await fetch(`${baseUrl}/find`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            sort: { postedAt: -1 },
+          }),
+        });
+        const readDataJson = await readData.json();
+        res.status(200).json(readDataJson.documents);
+        break;
+      case "POST":
+        const flutter = req.body;
+        const insertData = await fetch(`${baseUrl}/insertOne`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            document: flutter,
+          }),
+        });
+        const insertDataJson = await insertData.json();
+        res.status(200).json(insertDataJson);
+        break;
+      case "PUT":
+        const updateData = await fetch(`${baseUrl}/updateOne`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            filter: { _id: { $oid: req.body._id } },
+            update: {
+              $set: {
+                body: req.body.body,
+              },
+            },
+          }),
+        });
+        const updateDataJson = await updateData.json();
+        res.status(200).json(updateDataJson);
+        break;
+      case "DELETE":
+        const deleteData = await fetch(`${baseUrl}/deleteOne`, {
+          ...fetchOptions,
+          body: JSON.stringify({
+            ...fetchBody,
+            filter: { _id: { $oid: req.body._id } },
+          }),
+        });
+        const deleteDataJson = await deleteData.json();
+        res.status(200).json(deleteDataJson);
+        break;
       default:
         res.status(405).end();
         break;
